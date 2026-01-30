de626be36058b62821d1d67962fd6fa985f4cfc0
Nick <nicko.guimaraes@gmail.com>
2026-01-30 03:01:26 -0300

    [2.0.0] - update for 2.0

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,4 +1,6 @@
-use crate::handlers::simple_handler::get_pong;
+use crate::handlers::simple_handler::{
+    get_params, get_pong, get_question, post_body_data, protected_route,
+};
 use axum::{Router, routing::get, routing::post};
 use http::StatusCode;
 use std::time::Duration;
@@ -14,31 +16,31 @@ pub fn create_routes() -> Router {
     Router::new()
         .route("/ping", get(get_pong))
         .route("/protected-enter", get(protected_route))
-        .route("/params/:param_1/another_p/:param_2", get(get_params)) // localhost/params/1/another_p/textTest
+        .route("/params/{param_1}/another_p/{param_2}", get(get_params)) // localhost/params/1/another_p/textTest
         .route("/question_separator", get(get_question)) // localhost/question_separator?name=Jack&age=25&active=true
         .route("/body-data", post(post_body_data))
         .layer((
-        TraceLayer::new_for_http()
-            .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
-            .on_request(DefaultOnRequest::new().level(Level::INFO))
-            .on_response(
-                DefaultOnResponse::new()
-                    .level(Level::INFO)
-                    .latency_unit(tower_http::LatencyUnit::Micros),
-            )
-            .on_failure(
-                |failure_class: ServerErrorsFailureClass, latency: Duration, _: &Span| {
-                    tracing::error!(
-                        failure_class = ?failure_class,
-                        latency = ?latency,
-                        "request failed"
-                    );
-                },
-            ),
-        CompressionLayer::new(),
-        RequestBodyLimitLayer::new(1024 * 1024 * 10), // 10MB limit
-        TimeoutLayer::with_status_code(StatusCode::REQUEST_TIMEOUT, Duration::from_secs(60)),
-    ))
+            TraceLayer::new_for_http()
+                .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
+                .on_request(DefaultOnRequest::new().level(Level::INFO))
+                .on_response(
+                    DefaultOnResponse::new()
+                        .level(Level::INFO)
+                        .latency_unit(tower_http::LatencyUnit::Micros),
+                )
+                .on_failure(
+                    |failure_class: ServerErrorsFailureClass, latency: Duration, _: &Span| {
+                        tracing::error!(
+                            failure_class = ?failure_class,
+                            latency = ?latency,
+                            "request failed"
+                        );
+                    },
+                ),
+            CompressionLayer::new(),
+            RequestBodyLimitLayer::new(1024 * 1024 * 10), // 10MB limit
+            TimeoutLayer::with_status_code(StatusCode::REQUEST_TIMEOUT, Duration::from_secs(60)),
+        ))
 }
 
 //more than 1 route file? search for "axum merge routes"
